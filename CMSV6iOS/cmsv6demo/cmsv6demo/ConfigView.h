13a16436b91e2bac6fd0d0606a916678bbb67a7e
Story5 <337995545@qq.com>
2017-06-26 14:32:08 +0800

    import original native ios code and configure gitignore

---
+++ CMSV6iOS/cmsv6demo/cmsv6demo/ConfigView.h
@@ -0,0 +1,18 @@
+//
+//  ConfigView.h
+//  cmsv6demo
+//
+//  Created by 陶春节 on 2017/5/25.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConfigView : UIView
+
+@property (nonatomic,strong) UITextField *userName;
+@property (nonatomic,strong) UITextField *password;
+@property (nonatomic,strong) UITextField *ipTF;
+@property (nonatomic,strong) UITextField *deviceTF;
+
+@end
