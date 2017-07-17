feb49f9b1564cbcb4d0870b7e16f832b59551e13
Story5 <337995545@qq.com>
2017-07-17 16:47:38 +0800

    bower install ngCordova

--- ionic/src/pages/home/home.ts
+++ ionic/src/pages/home/home.ts
@@ -59,7 +59,7 @@ export class HomePage {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
     }
 
     this.camera.getPicture(options).then((imageData) => {
