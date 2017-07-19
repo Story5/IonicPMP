96ca414d33b31bde6efaa1c8b414c9d2779eb7af
Story5 <337995545@qq.com>
2017-07-20 00:03:16 +0800

    ionic

---
+++ blank/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { Broadcaster } from '@ionic-native/broadcaster';
+import { Camera } from '@ionic-native/camera';
+import { Device } from '@ionic-native/device';
+import { FileTransfer } from '@ionic-native/file-transfer';
+import { Keyboard } from '@ionic-native/keyboard';
+import { MediaCapture } from '@ionic-native/media-capture';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    BarcodeScanner,
+    Broadcaster,
+    Camera,
+    Device,
+    FileTransfer,
+    Keyboard,
+    MediaCapture,
+    ScreenOrientation
+  ]
+})
+export class AppModule {}
